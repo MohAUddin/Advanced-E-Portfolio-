8ec406d4df4a7a527948feeb504226adbc798169
MohAUddin <mohammad.uddin454@gmail.com>
2022-08-31 12:28:48 -0400

    added email response js function

--- index.js
+++ index.js
@@ -2,3 +2,26 @@
 // service_7k9v0y7
 // LnBFGGQS3aidVoJuh
 
+
+function contact() {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+
+    emailjs
+    .sendForm(
+        'service_7k9v0y7',
+        'template_b064kk4',
+        event.target,
+        'LnBFGGQS3aidVoJuh'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at muddin7067@gmail.com"
+        )
+    })
+}
